920b88e7e814a7cabed0484fe83cfe4754693e78
Alan Smith <commit@alanwsmith.com>
2022-05-20 17:32:22 -0400

    Working on basic setup

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -1,5 +1,23 @@
+;; Variables
+
+(defconst grimoire-buffer "*Grimoire*"
+  "Name of the Grimoire buffer")
+
+(defvar grimoire-file-list nil
+  "The list of files to display")
+
+;; Functions
+
+;; (defun grimoire-print-header ()
+;;   (insert "Grimoire\n\n")
+;;   )
+
+
+;; Framework
+
 (defvar grimoire-mode-hook nil
-  "*Add funcitons here*")
+  "Grimoire Hook"
+  )
 
 (defun grimoire-mode ()
   "Major mode for the an Org Mode Grimoire"
@@ -9,13 +27,16 @@
   (setq mode-name "Grimoire")
   (run-hooks 'grimoire-mode-hook))
 
+(put 'grimoire-mode 'mode-class 'special)
+
 (defun grimoire ()
   "Test to see if this provides grimoire"
   (interactive)
   (if (not (eq major-mode 'grimoire-mode))
       (grimoire-mode)
       )
-
   )
+
+
 
 (provide 'grimoire)
