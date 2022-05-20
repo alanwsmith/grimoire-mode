c28db82882c1167a9549c8e14ea31a6d800f9120
Alan Smith <commit@alanwsmith.com>
2022-05-20 19:32:01 -0400

    Basic setup of using minibuffer and putting the content selected in the buffer

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -8,10 +8,9 @@
 
 ;; Functions
 
-;; (defun grimoire-print-header ()
-;;   (insert "Grimoire\n\n")
-;;   )
-
+(defun grimoire-print-header ()
+  (insert "Grimoire\n\n")
+  )
 
 ;; Framework
 
@@ -27,14 +26,36 @@
   (setq mode-name "Grimoire")
   (run-hooks 'grimoire-mode-hook))
 
-(put 'grimoire-mode 'mode-class 'special)
+
+
+(defun grimoire-output ()
+  (switch-to-buffer grimoire-buffer)
+  (insert (grimoire-search))
+  )
+
+(defun grimoire-search ()
+  (interactive)
+  (completing-read
+   "Grimoire Search: "
+   `(("aexample_1" 1)
+     ("beaxmple_2" 2)
+     ("ceaxmple_2" 3)
+     ("deaxmple_2" 4)
+     )
+   )
+  )
+
+
 
 (defun grimoire ()
   "Test to see if this provides grimoire"
   (interactive)
+  (switch-to-buffer grimoire-buffer)
   (if (not (eq major-mode 'grimoire-mode))
       (grimoire-mode)
       )
+  (grimoire-output)
+
   )
 
 
