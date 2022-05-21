9f94894bbb8fee3d91f406e0b1b56bde40881e1c
Alan Smith <commit@alanwsmith.com>
2022-05-21 01:09:42 -0400

    Starting to use helm to get the finding to work

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -5,50 +5,110 @@
 
 ;; Framework
 
-(defvar grimoire-mode-hook nil
-  "Grimoire Hook"
-  )
+;; (defvar grimoire-mode-hook nil
+;;   "Grimoire Hook"
+;;   )
 
-(defun grimoire-mode ()
-  "Major mode for the an Org Mode Grimoire"
-  (message "Initializing Grimoire")
-  (kill-all-local-variables)
-  (setq major-mode 'grimoire-mode)
-  (setq mode-name "Grimoire")
-  (run-hooks 'grimoire-mode-hook))
+;; (defun grimoire-mode ()
+;;   "Major mode for the an Org Mode Grimoire"
+;;   ; (selectrum-mode +1)
+;;   (message "Initializing Grimoire")
+;;   (kill-all-local-variables)
+;;   (setq major-mode 'grimoire-mode)
+;;   (setq mode-name "Grimoire")
+;;   (run-hooks 'grimoire-mode-hook))
 
 
+;; (defun grimoire-search-completion-dynamic (prefix)
+;;   `("ax", "bx", "cx", "dx")
+;;   )
 
-(defun grimoire-output ()
-  (switch-to-buffer grimoire-buffer)
-  (insert (grimoire-search))
-  )
+;; (defun grimoire-search-completion (input predicate flag)
+;;   `("ax", "bx", "cx", "dx")
+;;   )
 
-(defun grimoire-search ()
+;; (defun grimoire-search-old-1 ()
+;;   (interactive)
+;;   (completing-read
+;;    "Grimoire Search: "
+;;    `grimoire-search-completion
+;;    )
+;;   )
+
+
+;; (defun grimoire-search ()
+;;   (interactive)
+;;   (selectrum-completing-read
+;;   ; (completing-read
+;;    "Grimoire Search: "
+;;    ; `selectrum-refine-candidates-function
+;;    `(("a" 1) ("b" 2) ("c" 3))
+;;    )
+;;   )
+
+;; (defun update-thing ()
+;;   (switch-to-buffer grimoire-buffer)
+;;   (insert "xxxxx")
+;;   )
+
+
+;; (defun grimoire-search-completion-selectrum (user-input current-list)
+;;   ; (update-thing)
+;;   `("ex", "fx", "gx", "hx")
+;;   )
+
+;; (setq selectrum-refine-candidates-function
+;;       #'grimoire-search-complection-selectrumx)
+
+
+;; (defun grimoire-search-function (prompt collection &optional
+;;             predicate require-match initial-input
+;;             hist def _inherit-input-method)
+;;   (switch-to-buffer grimoire-buffer)
+;;   (insert "HERE")
+;;       )
+
+; (setq completing-read-function 'grimoire-search-function)
+
+;; (defun grimoire-output ()
+;;   (switch-to-buffer grimoire-buffer)
+;;   (insert (completing-read
+;;            "Grimoire Search"
+;;            '(
+;;              ("a" 1)
+;;              ("b" 2)
+;;              ("c" 3)
+;;              )
+;;            )
+;;           )
+;;    )
+
+;; (defun grimoire ()
+;;   "Test to see if this provides grimoire"
+;;   (interactive)
+;;   (switch-to-buffer grimoire-buffer)
+;;   (if (not (eq major-mode 'grimoire-mode))
+;;       (grimoire-mode)
+;;       )
+;;   (grimoire-output)
+;;   )
+
+
+; (provide 'grimoire)
+
+
+(helm :sources (helm-build-async-source "aws-helm-source"
+                      :candidates-process
+                      (lambda ()
+                        (switch-to-buffer grimoire-buffer)
+                        (insert "asdf")
+                      (start-process "echo" nil "echo" "a\nb\nc\nd\ne")))
+      :buffer "*helm async source*")
+
+
+(defun aws-helm-test ()
   (interactive)
-  (completing-read
-   "Grimoire Search: "
-   `(("aexample_1" 1)
-     ("beaxmple_2" 2)
-     ("ceaxmple_2" 3)
-     ("deaxmple_2" 4)
-     )
-   )
-  )
+  (helm :sources 'aws-helm-source
+        :buffer "*helm my command*"))
 
 
-
-(defun grimoire ()
-  "Test to see if this provides grimoire"
-  (interactive)
-  (switch-to-buffer grimoire-buffer)
-  (if (not (eq major-mode 'grimoire-mode))
-      (grimoire-mode)
-      )
-  (grimoire-output)
-
-  )
-
-
-
-(provide 'grimoire)
