8cc788b88a08c7c03dc610106e969bcee55f4e04
Alan Smith <commit@alanwsmith.com>
2022-05-23 16:16:36 -0400

    Setup query to get overview form sample data for first result

---
+++ samples/set-2/post-query-get-details.bash
@@ -0,0 +1,11 @@
+curl -s \
+  	-X POST 'http://127.0.0.1:7575/indexes/movies/search' \
+    -H "Authorization: Bearer $(security find-generic-password -w -a alan -s alan--meilisearch--scratchpad--search-key)" \
+  	-H 'Content-Type: application/json' \
+    --data-binary '{ "q": "fox" }' \
+    | jq -r '.hits[0] | .overview'
+
+
+
+
+
