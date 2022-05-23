c616dbdc561b7549442a5cd140fa58f9a994fa3a
Alan Smith <commit@alanwsmith.com>
2022-05-23 14:44:17 -0400

    Setup another sample with async, but haven't gotten it to update the main window yet

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -140,6 +140,72 @@
 
 
 
+
+(defun grimoire-mode-search-helper (command)
+  "A function for the search helper"
+
+  ("a" "b" "c")
+
+  )
+
+
+
+
+
+(defun grimoire-mode-search-v2 ()
+  "This uses start-process which gets the list
+But I haven't figure out how to get it to update the
+text area too. Might be able to do it by passing to
+a function which builds an does the query and updates
+the main window then writes a list of candidates to
+a file that gets pulled back in via a new process.
+Would bascially be running two things, but it might work.
+Gonna try using 'volatile' in a basic 'helm-source' instead
+"
+  (setq meilisearch-auth-token
+        (string-clean-whitespace
+         (file-to-string "/Users/alan/configs/grimoire-mode/meilisearch-token")
+        )
+        )
+
+  (interactive)
+  (switch-to-buffer grimoire-buffer)
+  (insert
+   (helm :sources (helm-build-async-source "aws-helm-source"
+                   :candidates-process
+                   (lambda ()
+
+                     (setq curl-search-command
+                           (concat "curl -s -X POST 'http://127.0.0.1:7575/indexes/movies/search' -H 'Authorization: Bearer " 
+                                   meilisearch-auth-token
+                                   "' -H 'Content-Type: application/json' --data-binary '{ \"q\": "
+                                   "\"" helm-pattern  "\" }'  | jq -r '.hits[] | .title'"
+                                   )
+
+
+                           )
+
+                     (switch-to-buffer grimoire-buffer)
+                     ;'(grimoire-mode-search-helper)
+                     (start-process "bash" nil "/bin/bash" "-c"
+                                    curl-search-command
+                                    )
+                     ; (start-process "bash" nil '(grimoire-mod-search-helper))
+                     (grimoire-mode-search-helper)
+
+                      )
+
+                     )
+
+                   ))
+
+
+  )
+
+;; hard code to run
+; (grimoire-mode-search)
+
+
 (defun aws-helm-test()
   (setq meilisearch-auth-token
         (string-clean-whitespace
