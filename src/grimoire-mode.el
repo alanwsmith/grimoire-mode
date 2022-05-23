1d268f1049bcd8bf0422fb981b538372c01814dc
Alan Smith <commit@alanwsmith.com>
2022-05-23 17:22:29 -0400

    Working to grab the line the helm-buffer is on

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -59,7 +59,33 @@ list of items to show in the results"
 
 
 
-(defun grimoire-mode-search-v0.4 ()
+(defvar helm-buffer-line nil
+  "The line of the helm buffer")
+
+(defun post-line-move-hook ()
+  (switch-to-buffer helm-buffer)
+  (setq helm-buffer-line (with-current-buffer helm-buffer (format-mode-line "%l")))
+
+  (switch-to-buffer grimoire-buffer)
+
+  ;; (insert (format-mode-line "%l"))
+  ;; (insert "\n")
+  ;; (insert (format-mode-line "%l"))
+  ;; (insert "\n")
+  ;; (insert helm-buffer)
+
+  (insert "\n")
+  (insert helm-buffer-line)
+
+  ; (insert (with-current-buffer helm-buffer (format-mode-line "%l")))
+
+
+  )
+
+(setq helm-move-selection-after-hook 'post-line-move-hook)
+
+
+(defun grimoire-mode-search-v0.5 ()
   "This version makes two calls to the meilisearch search
 the first one returns the data for the file and the second one
 returns the next list of candidates"
@@ -92,10 +118,13 @@ returns the next list of candidates"
              meilisearch-auth-token
              helm-pattern)
 
+            (insert helm-buffer)
+
             ;; This first call updates the contents buffer
             (call-process "/bin/bash" nil "*Grimoire*" nil "-c"
                           curl-search-command-for-contents
                           )
+
 
             ;; This call gets the list of results to
             ;; set as the next list of candidates
