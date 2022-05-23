6b5896db4fee3468ca17816393613570be3449dc
Alan Smith <commit@alanwsmith.com>
2022-05-23 16:17:21 -0400

    Properly updating grimoire window for initial result (not yet when picking form the other results)

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -13,6 +13,10 @@
   "The curl command used to query meilisearch"
   )
 
+(defvar curl-search-command-contents nil
+  "For getting the contents for the top result"
+  )
+
 (defun file-to-string (file)
   "Read a file into a string"
   (with-temp-buffer
@@ -21,16 +25,28 @@
     )
   )
 
-(defun grimoire-mode-search-v0.3 ()
-  "This uses start-process which gets the list
-But I haven't figure out how to get it to update the
-text area too. Might be able to do it by passing to
-a function which builds an does the query and updates
-the main window then writes a list of candidates to
-a file that gets pulled back in via a new process.
-Would bascially be running two things, but it might work.
-Gonna try using 'volatile' in a basic 'helm-source' instead
-"
+
+(defun set-curl-search-command-for-contents (
+                                             auth-token
+                                             query-string)
+  "Build the curl command to get the content
+for the top search result"
+
+  (setq curl-search-command-for-contents
+        (concat "curl -s -X POST 'http://127.0.0.1:7575/indexes/movies/search' -H 'Authorization: Bearer " 
+                auth-token
+                "' -H 'Content-Type: application/json' --data-binary '{ \"q\": "
+                "\"" query-string "\" }'  | jq -r '.hits[0] | .overview'"
+                )
+        )
+  )
+
+
+
+(defun grimoire-mode-search-v0.4 ()
+  "This version makes two calls to the meilisearch search
+the first one returns the data for the file and the second one
+returns the next list of candidates"
   (setq meilisearch-auth-token
         (string-clean-whitespace
          (file-to-string "/Users/alan/configs/grimoire-mode/meilisearch-token")
@@ -38,7 +54,7 @@ Gonna try using 'volatile' in a basic 'helm-source' instead
         )
   (interactive)
   (switch-to-buffer grimoire-buffer)
-  (insert
+  (erase-buffer)
    (helm :sources (helm-build-async-source "aws-helm-source"
                    :candidates-process
                    (lambda ()
@@ -50,10 +66,15 @@ Gonna try using 'volatile' in a basic 'helm-source' instead
                                    )
                            )
                      (switch-to-buffer grimoire-buffer)
+                     (erase-buffer)
+                     (set-curl-search-command-for-contents meilisearch-auth-token helm-pattern)
+                     (call-process "/bin/bash" nil "*Grimoire*" nil "-c"
+                                    curl-search-command-for-contents
+                                    )
                      (start-process "bash" nil "/bin/bash" "-c"
                                     curl-search-command
                                     )
                       )
                      )
-                   ))
+                   )
   )
