9d3b5e170845d59534eda747a36067619bd4f51b
Alan Smith <commit@alanwsmith.com>
2022-05-25 18:33:05 -0400

    Clean up and point to local dir for search call

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -7,29 +7,26 @@
 
 (defun grimiore-mode-update-preview ()
   (switch-to-buffer grimoire-mode-buffer)
-  (insert "asdf")
   (erase-buffer)
   (call-process "/bin/bash" nil grimoire-mode-buffer nil "get-search-content" helm-pattern)
-  (goto-char (point-min))
-  )
+  (goto-char (point-min)))
 
 (defun grimoire-mode-search-v0.10 ()
   (interactive)
   (switch-to-buffer grimoire-mode-buffer)
+  (org-mode)
   (helm :sources
         (helm-build-async-source "Grimoire Search"
           :candidates-process
           (lambda ()
             (grimiore-mode-update-preview)
-            (start-process "search" nil "/bin/bash" "/Users/alan/workshop/grimoire-mode/src/search-process" helm-pattern)
-            )
-          )
-        :buffer "*helm grimoire search*"
-        )
-  (kill-buffer grimoire-mode-buffer)
-  )
-
-; This is an effort to reduce flickering
-(setq helm-input-idle-delay 0.01)
+            (start-process
+             "search"
+             nil
+             "/bin/bash"
+             "search-process"
+             helm-pattern)))
+        :buffer "*helm grimoire search*")
+  (kill-buffer grimoire-mode-buffer))
 
 (global-set-key [f5] 'grimoire-mode-search-v0.10)
