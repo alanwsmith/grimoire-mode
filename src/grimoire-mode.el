fe783ceb9adbac5716dafb009ed475f9daabf51c
Alan Smith <commit@alanwsmith.com>
2022-05-25 18:07:03 -0400

    Moved to single search script that returns Ready... text if there's no pattern

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -3,25 +3,27 @@
 (require 'helm-utils)
 
 
+(defconst grimoire-mode-buffer "*Grimoire*"
+  "Name of the Grimoire buffer")
+
+
+
 (defun grimoire-mode-search-v0.10 ()
   (interactive)
+  (switch-to-buffer grimoire-mode-buffer)
   (helm :sources
         (helm-build-async-source "Grimoire Search"
           :candidates-process
           (lambda ()
-            (if (string= helm-pattern "")
-                (start-process "bash"
-                               nil
-                               "/bin/bash"
-                               "-c"
-                               "echo 'Ready...'")
-              (start-process "search" nil "/bin/bash" "/Users/alan/workshop/grimoire-mode/src/search-process" helm-pattern)
-              )
+            (start-process "search" nil "/bin/bash" "/Users/alan/workshop/grimoire-mode/src/search-process" helm-pattern)
             )
           )
         :buffer "*helm grimoire search*"
         )
+  (kill-buffer grimoire-mode-buffer)
   )
 
+; This is an effort to reduce flickering
+(setq helm-input-idle-delay 0.01)
 
 (global-set-key [f5] 'grimoire-mode-search-v0.10)
