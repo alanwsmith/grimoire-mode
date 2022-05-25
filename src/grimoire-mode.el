17d815adc0b6e69faf3d233140aea620d81baf5f
Alan Smith <commit@alanwsmith.com>
2022-05-25 19:41:07 -0400

    Setup to use one function call for all the content calls (needs number adjusted)

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -5,11 +5,42 @@
 (defconst grimoire-mode-buffer "*Grimoire*"
   "Name of the Grimoire buffer")
 
+(defvar grimoire-mode-helm-buffer-line 0
+  "Storage for the current line position of
+the helm buffer")
+
+(defvar grimiore-mode-helm-buffer-line-adjusted 0
+  "Adjusted to accont for the fact the results
+start on the second line")
+
 (defun grimiore-mode-update-preview ()
+
+  (setq grimoire-mode-helm-buffer-line
+        (with-current-buffer helm-buffer (string-to-number (format-mode-line "%l")))
+        )
+
+  (setq grimoire-mode-helm-buffer-line-adjusted
+        grimoire-mode-helm-buffer-line
+        )
+
+  (message (number-to-string grimoire-mode-helm-buffer-line))
   (switch-to-buffer grimoire-mode-buffer)
   (erase-buffer)
-  (call-process "/bin/bash" nil grimoire-mode-buffer nil "get-search-content" helm-pattern)
+  (if (string= helm-pattern "")
+      (insert "-- Grimoire Mode --")
+    (call-process
+   "/bin/bash"
+   nil
+   grimoire-mode-buffer
+   nil
+   "get-search-content"
+   helm-pattern
+   (number-to-string grimoire-mode-helm-buffer-line-adjusted)
+   )
+  )
   (goto-char (point-min)))
+
+
 
 (defun grimoire-mode-search-v0.10 ()
   (interactive)
