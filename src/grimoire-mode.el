508650dc41f3e49d1bbe0b456f3808fe3daecc1a
Alan Smith <commit@alanwsmith.com>
2022-05-25 18:27:06 -0400

    Setup initial preview

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -2,11 +2,16 @@
 (require 'helm-lib)
 (require 'helm-utils)
 
-
 (defconst grimoire-mode-buffer "*Grimoire*"
   "Name of the Grimoire buffer")
 
-
+(defun grimiore-mode-update-preview ()
+  (switch-to-buffer grimoire-mode-buffer)
+  (insert "asdf")
+  (erase-buffer)
+  (call-process "/bin/bash" nil grimoire-mode-buffer nil "get-search-content" helm-pattern)
+  (goto-char (point-min))
+  )
 
 (defun grimoire-mode-search-v0.10 ()
   (interactive)
@@ -15,6 +20,7 @@
         (helm-build-async-source "Grimoire Search"
           :candidates-process
           (lambda ()
+            (grimiore-mode-update-preview)
             (start-process "search" nil "/bin/bash" "/Users/alan/workshop/grimoire-mode/src/search-process" helm-pattern)
             )
           )
