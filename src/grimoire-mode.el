089322925b8c27814bb6aa9127209ef67b467a46
Alan Smith <commit@alanwsmith.com>
2022-05-25 02:16:50 -0400

    Don't try to open a file if none was selected

--- src/grimoire-mode.el
+++ src/grimoire-mode.el
@@ -117,11 +117,15 @@ list of items to show in the results"
 (defun grimoire-mode-handle-result (return-value)
   "This is what handles the resturn value from the call"
   (setq helm-move-selection-after-hook nil)
-;  (switch-to-buffer grimoire-mode-buffer)
-;  (insert return-value)
-  ; (find-file "/Users/alan/Desktop/emacs-out.txt")
-  (find-file (concat "/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire/" return-value))
-  (org-mode)
+  (message (concat "Value: " return-value))
+  (if (string= return-value "Ready...")
+      (message "No file selected")
+    (progn
+      (message (concat "Loading: " return-value) )
+      (find-file (concat "/Users/alan/Library/Mobile Documents/com~apple~CloudDocs/Grimoire/" return-value))
+      (org-mode)
+      )
+      )
   (kill-buffer grimoire-mode-buffer)
   )
 
