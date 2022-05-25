7a19ba9556a2cb2b0fe0cf946685d68c27113547
Alan Smith <commit@alanwsmith.com>
2022-05-24 22:11:08 -0400

    Switched to port 7700

--- meilisearch-utils/start-meilisearch.bash
+++ meilisearch-utils/start-meilisearch.bash
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
+# Keeping dev on 7700 and will move prod to something else
 export MEILI_MASTER_KEY=$(security find-generic-password -w -a alan -s alan--meilisearch--scratchpad--root-key)
-export MEILI_HTTP_ADDR=127.0.0.1:7575
+export MEILI_HTTP_ADDR=127.0.0.1:7700
 export MEILI_ENV=development
 export MEILI_DB_PATH=/Users/alan/data/meilisearch-scratchpad/data.ms
 export MEILI_NO_ANALYTICS=true
