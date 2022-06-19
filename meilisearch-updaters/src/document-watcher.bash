60a700fbf6f3f4fc936f714f72b99d6d62d7ac4c
Alan Smith <commit@alanwsmith.com>
2022-06-19 02:08:40 -0400

    Changed file names

--- meilisearch-updaters/src/document-watcher.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-fswatch -0 "/Users/alan/Grimoire" | while read -d "" event
-  do
-      /Users/alan/workshop/grimoire-mode/meilisearch-updaters/src/document-updater.js "$event"
-  done
